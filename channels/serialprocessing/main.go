16a597b3e63c004f4ee5a7a190fd1beff9c967f7
Gaku Yasui <gaku.yasui@gmail.com>
2025-08-30 12:26:04 +0900

    update

--- channels/serialprocessing/main.go
+++ channels/serialprocessing/main.go
@@ -5,18 +5,16 @@ import (
 )
 
 func main() {
-	reqChan := make(chan Request)
-
-	// Create and start Worker
-	worker := Worker{ReqChan: reqChan}
-	go worker.Run()
+	worker := NewWorker()
+	worker.Start()
 
 	// Simulate multiple callers
 	for i := 1; i <= 3; i++ {
-		respChan := make(chan int)
-		req := Request{Value: i, RespChan: respChan}
-		reqChan <- req     // Send request with response channel
-		resp := <-respChan // Receive response from worker
+		resp, err := worker.Send(i)
+		if err != nil {
+			fmt.Printf("Request %d, Error: %v\n", i, err)
+			continue
+		}
 		fmt.Printf("Request %d, Response %d\n", i, resp)
 	}
 }
@@ -29,10 +27,25 @@ type Request struct {
 // Worker struct holds the request channel
 type Worker struct {
 	ReqChan chan Request
+	started chan struct{}
 }
 
-// Run method processes incoming requests
-func (w *Worker) Run() {
+// NewWorker function initializes a Worker
+func NewWorker() *Worker {
+	return &Worker{
+		ReqChan: make(chan Request),
+		started: make(chan struct{}),
+	}
+}
+
+// Start method to start the worker
+func (w *Worker) Start() {
+	go w.run()
+	<-w.started
+}
+
+func (w *Worker) run() {
+	close(w.started)
 	for req := range w.ReqChan {
 		// Process the request and send response
 		resp := req.Value * 2 // Example: double the value
@@ -40,10 +53,22 @@ func (w *Worker) Run() {
 	}
 }
 
-func worker(reqChan chan Request) {
-	for req := range reqChan {
-		// Process the request and send response
-		resp := req.Value * 2 // Example: double the value
-		req.RespChan <- resp  // Send response to the specific channel
+func (w *Worker) checkStarted() error {
+	select {
+	case <-w.started:
+		return nil
+	default:
+		return fmt.Errorf("Worker.Start() has not been called")
 	}
+}
+
+func (w *Worker) Send(value int) (int, error) {
+	if err := w.checkStarted(); err != nil {
+		return 0, err
+	}
+	respChan := make(chan int)
+	req := Request{Value: value, RespChan: respChan}
+	w.ReqChan <- req
+	resp := <-respChan
+	return resp, nil
 }
