25cd95f7234f5c1ce3d871b7c763aadce00a97fe
Gaku Yasui <gaku.yasui@gmail.com>
2025-09-13 13:13:03 +0900

    add pipeline

---
+++ pipeline/go.mod
@@ -0,0 +1,3 @@
+module github.com/gkuga/go-examples/pipeline
+
+go 1.23.10
---
+++ pipeline/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+type Done <-chan interface{}
+type Stream <-chan int
+
+func generator(done Done, nums ...int) Stream {
+	intStream := make(chan int, len(nums))
+	go func() {
+		defer close(intStream)
+		for _, n := range nums {
+			select {
+			case <-done:
+				return
+			case intStream <- n:
+			}
+		}
+	}()
+	return intStream
+}
+
+func multiply(done Done, intStream Stream, multiplier int) Stream {
+	multipliedStream := make(chan int)
+	go func() {
+		defer close(multipliedStream)
+		for n := range intStream {
+			select {
+			case <-done:
+				return
+			case multipliedStream <- n * multiplier:
+			}
+		}
+	}()
+	return multipliedStream
+}
+
+func add(done Done, intStream Stream, additive int) Stream {
+	addedStream := make(chan int)
+	go func() {
+		defer close(addedStream)
+		for n := range intStream {
+			select {
+			case <-done:
+				return
+			case addedStream <- n + additive:
+			}
+		}
+	}()
+	return addedStream
+}
+
+func main() {
+	// This is just a placeholder main function.
+	done := make(chan interface{})
+	defer close(done)
+
+	nums := []int{1, 2, 3, 4, 5}
+	intStream := generator(done, nums...)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+	for result := range pipeline {
+		fmt.Println(result)
+	}
+}
